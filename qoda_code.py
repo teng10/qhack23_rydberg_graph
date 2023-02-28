6c6bacaaa21c2dcb3d5a3479a0a459c1e99d4fce
Hrushikesh Patil <hrushikeshpatil890@gmail.com>
2023-02-27 23:43:19 -0500

    qoda code cleaned up

--- qoda_code.py
+++ qoda_code.py
@@ -8,23 +8,23 @@ def final_circuit_ansatz(fixed_params, var_par, graph_node_list, graph_edges_lis
 
     for k in range(0, repeatations):
         for node in range(v):
-            for i in range(params.shape[0]):
+            for i in range(var_par.shape[0]):
                 if i % 2 == 0:
-                    kernel.rx(parameter = param_fixed[i]*graph_node_feat[node][i], target = qubits[node])
+                    kernel.rx(parameter = fixed_params[i]*graph_node_feat[node][i], target = qubits[node])
                 if i % 2 == 1:
-                    kernel.rx(parameter = param_fixed[i]*graph_node_feat[node][i], target = qubits[node])
+                    kernel.rx(parameter = fixed_params[i]*graph_node_feat[node][i], target = qubits[node])
         
         for i in range(v):
-            kernel.rz(parameter = var_par[3*qubit], target = qubits[i])
-            kernel.ry(parameter = var_par[3*qubit+1], target = qubits[i])
-            kernel.rz(parameter = var_par[3*qubit+2], target = qubits[i])
+            kernel.rz(parameter = var_par[3*v], target = qubits[i])
+            kernel.ry(parameter = var_par[3*v+1], target = qubits[i])
+            kernel.rz(parameter = var_par[3*v+2], target = qubits[i])
         
         for edge in graph_edges_list:
             kernel.z(target = qubits[edge[0]])
             kernel.z(target = qubits[edge[1]])
 
         for q in range(v + 1):
-            kernel.cx(control = qubits[q], target = qubits[(qubit+1)%(V+1)])
+            kernel.cx(control = qubits[q], target = qubits[(q+1)%(v+1)])
     kernel.mz(qubits)
     return kernel
 
@@ -34,6 +34,7 @@ def final_circuit_ansatz(fixed_params, var_par, graph_node_list, graph_edges_lis
 # graph_edges_list =
 # graph_node_feat =
 
+# @ Steffano Need help here especially what is node and what does final_cost returns
 def final_cost(subgraphs, params_init, var_param):
     subgraph=subgraphs[node]["subgraph"]
     sub_node, sub_edge= subgraphs[node]["subgraph"]
